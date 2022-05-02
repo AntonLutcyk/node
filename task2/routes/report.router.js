daa919df65737efa0eab10139aec5ffb5801039b
AntonLutcyk <97983854+AntonLutcyk@users.noreply.github.com>
2022-05-02 16:40:55 +0300

    Delete task2 directory

--- task2/routes/report.router.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-
-const reportController = require('../contollers/report.controller')
-
-const reportRouter = Router();
-module.exports = reportRouter;
-
-reportRouter.get('/', reportController.getReports);
-
