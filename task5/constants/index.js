6a5cbff7da94eb64931177c201cd47eae0024db7
AntonLutcyk <97983854+AntonLutcyk@users.noreply.github.com>
2022-05-13 18:25:21 +0300

    Delete task5 directory

--- task5/constants/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  userRolesEnum: require('./user-roles.enum'),
-}
--- task5/routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  userRouter: require('./user.router'),
-  reportRouter: require(`./report.router`),
-  carRouter: require('./cars.router')
-}
